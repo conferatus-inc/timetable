7b8b111aec6300e37437b1578c0317ef4054b631
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-10-31 04:13:10 +0700

    add exceptions, example of completable future and other

---
+++ backend/src/main/java/org/conferatus/timetable/backend/exception/TypicalServerExceptions.java
@@ -0,0 +1,56 @@
+package org.conferatus.timetable.backend.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+public enum TypicalServerExceptions {
+    NOT_FOUND_EXCEPTION(new ServerException(HttpStatus.NOT_FOUND, "NOT_FOUND_EXCEPTION")),
+    ACCESS_TOKEN_EXPIRED(new ServerException(HttpStatus.UNAUTHORIZED, "ACCESS_TOKEN_EXPIRED")),
+    REFRESH_TOKEN_EXPIRED(new ServerException(HttpStatus.UNAUTHORIZED, "REFRESH_TOKEN_EXPIRED")),
+    ILLEGAL_ACCESS_TOKEN(new ServerException(HttpStatus.UNAUTHORIZED, "ILLEGAL_ACCESS_TOKEN")),
+    ILLEGAL_REFRESH_TOKEN(new ServerException(HttpStatus.UNAUTHORIZED, "ILLEGAL_REFRESH_TOKEN")),
+    UNAUTHORIZED(new ServerException(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED")),
+    INVALID_PARAMETER(new ServerException(HttpStatus.BAD_REQUEST, "INVALID_PARAMETER")),
+    BAD_REQUEST(new ServerException(HttpStatus.BAD_REQUEST)),
+    NO_ACCESS_TOKEN(new ServerException(HttpStatus.UNAUTHORIZED, "NO_ACCESS_TOKEN")),
+    ACCESS_TOKEN_PROBLEM(new ServerException(HttpStatus.UNAUTHORIZED, "ACCESS_TOKEN_PROBLEM")),
+
+    ;
+
+    private final ServerException serverException;
+
+    TypicalServerExceptions(HttpStatus httpStatus, String message, String moreInfo) {
+        serverException = new ServerException(httpStatus, message, moreInfo);
+    }
+
+    TypicalServerExceptions(ServerException serverException) {
+        this.serverException = serverException;
+    }
+
+    public TypicalServerExceptions message(String message) {
+        this.serverException.setCode(message);
+        return this;
+    }
+
+    public TypicalServerExceptions moreInfo(String moreInfo) {
+        this.serverException.setMessage(moreInfo);
+        return this;
+    }
+
+    public void throwException() {
+        throw serverException;
+    }
+
+    public ServerException getServerException() {
+        return serverException;
+    }
+
+    public Map<String, Object> getAnswer() {
+        return serverException.getAnswer();
+    }
+
+    public int status() {
+        return serverException.status.value();
+    }
+}
