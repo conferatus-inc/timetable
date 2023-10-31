cfe837731d58a3cd2e3fc0e505e1d3ca5268d4b0
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-11-01 04:42:23 +0700

    стикер с томасом шелби бебра похоронена чупапи муняня настало твое время

---
+++ frontend/frontend-user/src/components/ItemsButtonsList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-for="item in items" :key="item.id">
+      <v-spacer></v-spacer>
+      <v-btn 
+      variant="outlined" 
+      class="ma-1"
+      >
+        {{ item.name }}
+      </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'items-buttons-list',
+  props: {
+    items: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ frontend/frontend-user/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <v-container>
+    <v-row>
+      <v-col>
+        <h2 class="ma-1">Добро пожаловать!</h2>
+
+        <v-spacer></v-spacer>
+
+        <v-btn 
+        variant="outlined" 
+        class="ma-1"
+        @click="$router.push({ path: '/group' })"
+        >
+          Расписание для группы
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn 
+        variant="outlined" 
+        class="ma-1"
+        @click="$router.push({ path: '/auditory' })"
+        >
+          Расписание для аудитории
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn 
+        variant="outlined" 
+        class="ma-1"
+        @click="$router.push({ path: '/teacher' })"
+        >
+          Расписание для преподавателя
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+  // import HelloWorld from '@/components/HelloWorld.vue'
+  import { ref } from 'vue'
+ 
+</script>
