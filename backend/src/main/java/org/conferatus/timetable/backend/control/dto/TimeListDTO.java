01f006a14e81d46479531170b817fa6c5340fff1
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-11-01 15:14:55 +0700

    rename and other

--- backend/src/main/java/org/conferatus/timetable/backend/control/dto/TimeListDTO.java
+++ backend/src/main/java/org/conferatus/timetable/backend/control/dto/TimeListDTO.java
@@ -3,13 +3,12 @@ package org.conferatus.timetable.backend.control.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
-import java.util.Map;
 
-public record TimeTableDTO(
+public record TimeListDTO(
         @JsonProperty("days_name") List<String> days,
         @JsonProperty("study_days_per_week") int daysPerWeek,
         @JsonProperty("lessons_per_day") int lessonsPerDay,
-        Map<String, Map<Integer, LessonDTO>> lessonsTable
+        List<LessonDTO> lessonsList
 ) {
 
 
