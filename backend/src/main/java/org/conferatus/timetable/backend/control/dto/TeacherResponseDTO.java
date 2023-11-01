454ea181bf41fabcb944b58519342a3446ad4548
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-11-01 14:38:02 +0700

    add table service

--- backend/src/main/java/org/conferatus/timetable/backend/control/dto/TeacherResponseDTO.java
+++ backend/src/main/java/org/conferatus/timetable/backend/control/dto/TeacherResponseDTO.java
@@ -1,8 +1,14 @@
 package org.conferatus.timetable.backend.control.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.conferatus.timetable.backend.model.entity.Teacher;
 
 public record TeacherResponseDTO(
+        @JsonProperty("id") Long id,
         @JsonProperty("name") String name
 ) {
+    public TeacherResponseDTO(Teacher teacher) {
+        this(teacher.getId(),
+                teacher.getName());
+    }
 }
