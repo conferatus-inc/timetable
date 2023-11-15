41dbe020be4edd00d18f263191c302f1e6791ce6
ttrubinov <t.trubinov@g.nsu.ru>
2023-11-15 10:15:25 +0300

    Fixed DTO

--- backend/src/main/java/org/conferatus/timetable/backend/control/dto/SubjectDTO.java
+++ backend/src/main/java/org/conferatus/timetable/backend/control/dto/SubjectDTO.java
@@ -1,23 +1,22 @@
 package org.conferatus.timetable.backend.control.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.conferatus.timetable.backend.model.entity.Lesson;
 import org.conferatus.timetable.backend.model.entity.Subject;
-import org.conferatus.timetable.backend.model.entity.Teacher;
 
-public record SubjectDTO (
-    Long id,
-    String name,
-    List<Teacher> possibleTeacher,
-    List<Lesson> lessons
+public record SubjectDTO(
+        Long id,
+        String name,
+        List<TeacherResponseDTO> possibleTeacher,
+        List<LessonDTO> lessons
 ) {
     public SubjectDTO(Subject subject) {
         this(
                 subject.getId(),
                 subject.getName(),
-                subject.getPossibleTeacher(),
-                subject.getLessons()
+                subject.getPossibleTeacher().stream().map(TeacherResponseDTO::new).collect(Collectors.toList()),
+                subject.getLessons().stream().map(LessonDTO::new).collect(Collectors.toList())
         );
     }
 }
