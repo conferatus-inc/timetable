538787b59088149fdeb76674bf22bba18ad4e8bd
ttrubinov <t.trubinov@g.nsu.ru>
2023-11-29 11:44:31 +0700

    Possible subjects for teacher

--- backend/src/main/java/org/conferatus/timetable/backend/model/entity/Teacher.java
+++ backend/src/main/java/org/conferatus/timetable/backend/model/entity/Teacher.java
@@ -1,10 +1,21 @@
 package org.conferatus.timetable.backend.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
@@ -16,9 +27,18 @@ public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "teacher_subject",
+            joinColumns = @JoinColumn(name = "teacher_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "subject_id", referencedColumnName = "id")
+    )
+    private List<Subject> possibleSubjects = new ArrayList<>();
 
     @OneToMany
     @JoinColumn(name = "lesson_id")
-    List<Lesson> lessons = new ArrayList<>();
+    private List<Lesson> lessons = new ArrayList<>();
 }
