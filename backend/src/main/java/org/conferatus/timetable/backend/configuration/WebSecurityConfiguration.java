e60c5a6648acd70174e43e9262bec30def8ff969
ttrubinov <t.trubinov@g.nsu.ru>
2023-11-29 15:45:34 +0700

    Auth turned off

--- backend/src/main/java/org/conferatus/timetable/backend/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/org/conferatus/timetable/backend/configuration/WebSecurityConfiguration.java
@@ -3,16 +3,16 @@ package org.conferatus.timetable.backend.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+//@EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfiguration //implements WebSecurityConfigurerAdapter
 {
@@ -20,12 +20,17 @@ public class WebSecurityConfiguration //implements WebSecurityConfigurerAdapter
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-//                .authorizeHttpRequests(authz -> authz.requestMatchers("/admin").authenticated()) // TODO
-//                .httpBasic(withDefaults())                                                       // TODO
-                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry.anyRequest().permitAll());
+//                    authorizeRequests(
+//                    value
+//            ).antMatchers("/**").permitAll();
+
+
         return http.build();
     }
-
-
 }
