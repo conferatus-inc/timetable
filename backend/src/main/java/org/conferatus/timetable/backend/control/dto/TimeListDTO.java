1fdd6014fcac02455f465fa9d71e0cf8b8ccfe18
ttrubinov <t.trubinov@g.nsu.ru>
2023-11-30 20:59:48 +0700

    Renaming

--- backend/src/main/java/org/conferatus/timetable/backend/control/dto/TimeListDTO.java
+++ backend/src/main/java/org/conferatus/timetable/backend/control/dto/TimeListDTO.java
@@ -1,14 +1,14 @@
 package org.conferatus.timetable.backend.control.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record TimeListDTO(
         @JsonProperty("days_name") List<String> days,
         @JsonProperty("study_days_per_week") int daysPerWeek,
         @JsonProperty("lessons_per_day") int lessonsPerDay,
-        List<LessonDTO> lessonsList
+        List<LessonDTO> cells
 ) {
 
 
