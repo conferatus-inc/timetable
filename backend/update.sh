baf4fce76f688dc25be3201ff39507054e759b64
trubinovta <t.trubinov@g.nsu.ru>
2024-04-10 00:04:25 +0700

    try action

--- backend/update.sh
+++ backend/update.sh
@@ -3,6 +3,6 @@
 cd /tmp;
 ls -al;
 sudo docker ps;
-sudo docker stop timetable_container || true;
+sudo docker stop timetable_container;
 sudo docker load -i timetable_backend.tar;
 sudo docker run --rm -d -p 8080:8080 --name timetable_container timetable_backend:latest;
