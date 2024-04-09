e86f050e3aec38a7f39500d1e1b0dabf4d8ad409
trubinovta <t.trubinov@g.nsu.ru>
2024-04-10 00:06:29 +0700

    port fix

--- backend/update.sh
+++ backend/update.sh
@@ -5,4 +5,4 @@ ls -al;
 sudo docker ps;
 sudo docker stop timetable_container;
 sudo docker load -i timetable_backend.tar;
-sudo docker run --rm -d -p 8080:8080 --name timetable_container timetable_backend:latest;
+sudo docker run --rm -d -p 8090:8090 --name timetable_container timetable_backend:latest;
