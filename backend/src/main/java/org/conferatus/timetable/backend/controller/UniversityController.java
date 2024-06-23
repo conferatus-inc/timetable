e677e5e82c0efced0d2b594b50b83edbceeb152e
trubinovta <t.trubinov@g.nsu.ru>
2024-06-23 15:43:07 +0700

    fix

--- backend/src/main/java/org/conferatus/timetable/backend/controller/UniversityController.java
+++ backend/src/main/java/org/conferatus/timetable/backend/controller/UniversityController.java
@@ -6,7 +6,6 @@ import org.conferatus.timetable.backend.dto.UserDto;
 import org.conferatus.timetable.backend.services.UniversityService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,7 @@ public class UniversityController {
     private final UniversityService universityService;
 
     @PostMapping
-    public ResponseEntity<UniversityDto> createUniversity(@RequestBody String name) {
+    public ResponseEntity<UniversityDto> createUniversity(@RequestParam String name) {
         return ResponseEntity.ok(new UniversityDto(universityService.createUniversity(name)));
     }
 
