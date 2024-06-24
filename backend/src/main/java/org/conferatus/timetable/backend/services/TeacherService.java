91e42e66b632c07c2dd9a3d41c32962b896c8916
trubinovta <t.trubinov@g.nsu.ru>
2024-06-25 00:59:15 +0700

    fix npe teachers

--- backend/src/main/java/org/conferatus/timetable/backend/services/TeacherService.java
+++ backend/src/main/java/org/conferatus/timetable/backend/services/TeacherService.java
@@ -1,5 +1,7 @@
 package org.conferatus.timetable.backend.services;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.conferatus.timetable.backend.dto.TeacherWishDto;
 import org.conferatus.timetable.backend.exception.ServerException;
@@ -11,8 +13,6 @@ import org.conferatus.timetable.backend.repository.TeacherRepository;
 import org.conferatus.timetable.backend.repository.TeacherWishRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -69,7 +69,7 @@ public class TeacherService {
 
     public List<Teacher> getAllTeachers(University university) {
         return teacherRepository.findAll().stream()
-                .filter(it -> it.getUniversity() != null && it.getUniversity().id().equals(university.id())).toList();
+                .filter(it -> it.getUniversity() != null && university != null && it.getUniversity().id().equals(university.id())).toList();
     }
 
     public Teacher addTeacher(User user, String teacherName) {
