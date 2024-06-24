9607d0f70eaa3c304062b6cb658b55af1955b94d
trubinovta <t.trubinov@g.nsu.ru>
2024-06-24 17:31:11 +0700

    fix cors

--- backend/src/main/java/org/conferatus/timetable/backend/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/org/conferatus/timetable/backend/configuration/WebSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package org.conferatus.timetable.backend.configuration;
 
-import java.util.Arrays;
 import java.util.Set;
 
 import lombok.RequiredArgsConstructor;
@@ -12,9 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -50,18 +46,6 @@ public class WebSecurityConfiguration implements WebMvcConfigurer {
 
         return http.build();
     }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
